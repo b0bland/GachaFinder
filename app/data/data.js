7944fa9bd422c73c07e16ad999ee623553a66464
b0bland <alfredychan96@gmail.com>
2019-07-02 02:15:25 -0700

    Added data

---
+++ app/data/data.js
@@ -0,0 +1,26 @@
+var gachalist = [
+    {
+        name: "Fate/Grand Order",
+        values: [5,2,4,5,1,3]
+    },
+    {
+        name: "Azur Lane",
+        values: [1,4,3,3,5,4]
+    },
+    {
+        name: "Girls' Frontline",
+        values: [3,3,5,3,2,3]
+    },
+    {
+        name: "Granblue Fantasy",
+        values: [4,1,4,4,4,4]
+    },
+    {
+        name: "Kirara Fantasia",
+        values: [2,5,3,2,4,5]
+    },
+    {
+        name: "BanG Dream!",
+        values: [4,2,1,4,1,3]
+    }
+]
