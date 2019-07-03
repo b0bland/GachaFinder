2162a8ce722351bed99bc04775f37450b9b7c52a
b0bland <alfredychan96@gmail.com>
2019-07-03 01:45:51 -0700

    Comparison of values between data and input

--- app/data/data.js
+++ app/data/data.js
@@ -1,26 +1,40 @@
-var gachalist = [
+var gachaList = [
     {
         name: "Fate/Grand Order",
-        values: [5,2,4,5,1,3]
+        values: [5,2,4,5,1,3],
+        imageURL: "https://upload.wikimedia.org/wikipedia/en/4/42/Fate_Grand_Order_logo.png",
+        id: 0
     },
     {
         name: "Azur Lane",
-        values: [1,4,3,3,5,4]
+        values: [1,4,3,3,5,4],
+        imageURL: "https://upload.wikimedia.org/wikipedia/en/c/c6/Azur_Lane_English_Release_Logo.png",
+        id: 1
     },
     {
         name: "Girls' Frontline",
-        values: [3,3,5,3,2,3]
+        values: [3,3,5,3,2,3],
+        imageURL: "https://upload.wikimedia.org/wikipedia/commons/d/d2/I13682814964.png",
+        id: 2
     },
     {
         name: "Granblue Fantasy",
-        values: [4,1,4,4,4,4]
+        values: [4,1,4,4,4,4],
+        imageURL: "https://upload.wikimedia.org/wikipedia/en/e/e5/Granblue_Fantasy_logo.png",
+        id: 3
     },
     {
         name: "Kirara Fantasia",
-        values: [2,5,3,2,4,5]
+        values: [2,5,3,2,4,5],
+        imageURL: "https://kirarafantasia.com/wp-content/themes/kirara/img/common/logo.svg",
+        id: 4
     },
     {
         name: "BanG Dream!",
-        values: [4,2,1,4,1,3]
+        values: [4,2,1,4,1,3],
+        imageURL: "https://bang-dream-gbp-en.bushiroad.com/img/bangdream_logo.png",
+        id: 5
     }
 ]
+
+module.exports = gachaList;
